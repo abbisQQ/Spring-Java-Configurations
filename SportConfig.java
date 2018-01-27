79a782ae45d2af20c16f556f4449d2fce7731dd5
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-01-27 16:57:04 +0200

    Create SportConfig.java

---
+++ SportConfig.java
@@ -0,0 +1,15 @@
+package com.spring.tutorials;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//annotation for the configuration file and also to scan for components inside our packages
+@Configuration
+@ComponentScan("com.spring.tutorials")
+public class SportConfig {
+
+	
+	
+	
+	
+}
